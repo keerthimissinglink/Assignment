680fd2f0b5d6a2eda7d0894ec5bccbda3585f25f
p.keerthi <p.keerthi@RILLPT1574>
2021-07-23 18:24:47 +0530

    dynamic data in card

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,15 +10,19 @@ import { apiService } from '../services/api.service';
 export class DashboardComponent implements OnInit {
   displayedColumns: string[] = ['state', 'confirmed', 'recovered', 'deaths'];
   dataSource:any;
-  total;
-  
+  total=[];
+
  
   constructor(private apiService : apiService,private route:Router) { }
 
   ngOnInit() {
     this.apiService.getState().subscribe(data => {
       this.dataSource = data.statewise.filter(e=> e.state != 'Total' && e.statecode != 'UN');
-      this.total = data.statewise.find(e => e.state == 'Total');
+      const total = data.statewise.find(e => e.state == 'Total');
+      this.total.push({name:'confirmed',cnt:total.confirmed,style:'confirmed'})
+      this.total.push({name:'recovered',cnt:total.recovered,style:'recovered'})
+      this.total.push({name:'deaths',cnt:total.deaths,style:'deaths'})
+      console.log(this.total)
       console.log(this.dataSource)
     });
     
